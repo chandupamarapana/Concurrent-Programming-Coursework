1796419023230ed4f9122e8c3904565257f8eb0d
Chandupa Marapana <136840067+chandupamarapana@users.noreply.github.com>
2026-01-02 02:29:17 +0530

    [commit-1] - adding old submissions system simulator code

--- src/org/example/ExamSubmissionSimulator.java
+++ src/org/example/ExamSubmissionSimulator.java
@@ -2,6 +2,40 @@ package org.example;
 
 public class ExamSubmissionSimulator {
     public static void main(String[] args) {
+        System.out.println("EASTMINSTER UNIVERSITY - EXAM SUBMISSION COMPARISON");
+
+        // first test with smaller number first to see the difference clearly
+        int testStudents = 1000;
+
+        int poolsize = Runtime.getRuntime().availableProcessors()*2;
+
+        System.out.println("Simulator Configuration");
+        System.out.println("number of students: "+ testStudents);
+        System.out.println("Thread pool size "+ poolsize);
+        System.out.println();
+
+
+        //test old system
+        System.out.println(" The old system ");
+        System.out.println();
+
+        OldSubmissionSystem oldSubmissionSystem = new OldSubmissionSystem(testStudents, poolsize);
+        long oldStartTime = System.currentTimeMillis();
+
+        oldSubmissionSystem.processSubmissions();
+
+        long oldEndTime = System.currentTimeMillis();
+        long oldTotalTime = oldEndTime - oldStartTime;
+
+        System.out.println(" Old system results ");
+        System.out.println();
+        oldSubmissionSystem.getSubmissionStats().displayStats();
+        System.out.println(" Actual total time in ms: "+ oldTotalTime);
+        System.out.println();
+
+        //calculate average time per student
+        double oldTotalTimeDouble = (double) oldTotalTime;
+        double averageWaitTime = oldTotalTimeDouble / testStudents;
 
 
     }
